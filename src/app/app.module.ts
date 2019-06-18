75c7a21a43b6a1794398c4074137d4aded27926d
ranganathanselvan <ranganathanselvan@gmail.com>
2019-06-19 01:24:53 +0530

    new component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BillCreateComponent } from './bill/bill-create/bill-create.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BillCreateComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/bill/bill-create/bill-create.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-bill-create',
+  templateUrl: './bill-create.component.html'
+})
+export class BillCreateComponent {}
