88669db706a369ef39563f6f1e081cbce9ae9e0e
ranganathanselvan <ranganathanselvan@gmail.com>
2019-06-20 00:12:27 +0530

    added new components and modified bill create component

--- src/app/bill/bill-create/bill-create.component.ts
+++ src/app/bill/bill-create/bill-create.component.ts
@@ -4,4 +4,39 @@ import { Component } from '@angular/core';
   selector: 'app-bill-create',
   templateUrl: './bill-create.component.html'
 })
-export class BillCreateComponent {}
+export class BillCreateComponent {
+  enteredShopName = '';
+  enteredDate = '';
+  enteredCashier = '';
+  enteredProductCode = '';
+  enteredProductName = '';
+  enteredPrice = '';
+  enteredQuantity = '';
+  enteredTotalPrice = '';
+  enteredProduct = [];
+
+  onAddProduct() {
+    let productObj = {
+      ProductCode: this.enteredProductCode, ProductName: this.enteredProductName,
+      Price: this.enteredPrice, Quantity: this.enteredQuantity, TotalPrice: this.enteredTotalPrice
+    };
+    this.enteredProduct.push(productObj);
+    productObj = {
+      ProductCode: '', ProductName: '',
+      Price: '', Quantity: '', TotalPrice: ''
+    };
+    this.enteredProductCode = '';
+    this.enteredProductName = '';
+    this.enteredPrice = '';
+    this.enteredQuantity = '';
+    this.enteredTotalPrice = '';
+  }
+  onClearProduct() {
+    this.enteredProduct = [];
+    this.enteredProductCode = '';
+    this.enteredProductName = '';
+    this.enteredPrice = '';
+    this.enteredQuantity = '';
+    this.enteredTotalPrice = '';
+  }
+}
