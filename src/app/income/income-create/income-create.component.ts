395710a9a0ca42ffe934975005a5e865a9652cf0
ranganathanselvan <ranganathanselvan@gmail.com>
2019-07-12 00:41:19 +0530

    Adding routes

---
+++ src/app/income/income-create/income-create.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-create-income',
+  templateUrl: './income-create.component.html'
+})
+export class CreateIncomeComponent { }
