b479e31387bb1e8b673f457993e1271a2f840499
ranganathanselvan <ranganathanselvan@gmail.com>
2019-07-20 23:58:42 +0530

    added nav and pop up model

--- src/app/bill/bill-view/bill-view.component.ts
+++ src/app/bill/bill-view/bill-view.component.ts
@@ -8,14 +8,20 @@ import { BillService } from '../../shared/bill.service';
   providers: [BillService]
 })
 export class BillViewComponent implements OnInit {
-  billData = new Object();
+  billData = new Array();
+  selectedBill = {};
 
   constructor(private billService: BillService) { }
 
   ngOnInit() {
     this.billService.getAllBill().subscribe((res) => {
-      this.billData = res;
+      this.billData = Object.keys(res).map(i => res[i]);
       console.log(JSON.stringify(res));
     });
+  }
+
+  getRowIndex(index) {
+    console.log('Row Index: ' + index);
+    this.selectedBill = this.billData[index];
   }
 }
